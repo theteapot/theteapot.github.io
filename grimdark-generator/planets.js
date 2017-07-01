af3373c6828d59cce8dc354cef211d127a188505
theteapot <tin.teapot@gmail.com>
2017-07-02 05:45:54 +1200

    added some data for random names

---
+++ grimdark-generator/planets.js
@@ -0,0 +1,90 @@
+var cheerio = require("cheerio")
+var fs = require("fs")
+
+function loadFile(fileName, callback) {
+    fs.readFile(fileName, function (err, data) {
+        if (err) {
+            throw err;
+        }
+        callback(data)
+    })
+}
+
+function parseHtml(selector, data) {
+    var planetObj = {"planetNames":[]}
+    var $ = cheerio.load(data)
+    $(selector).each(function() {
+        planetObj.planetNames.push(($(this).text()))
+    })
+    return planetObj
+}
+
+function writeJSON(object, fileName) {
+    fs.writeFile(fileName+".json", JSON.stringify(object), function(err) {
+        if(err) {
+            return console.log(err)
+        }
+        console.log("Wrote JSON object as: "+fileName+".json")
+    })
+}
+
+function randomPlanet(jsonData) {
+    var planetObj = JSON.parse(jsonData)
+    var length = planetObj.planetNames.length
+
+    var planet1 = planetObj.planetNames[randomInt(0,length)]
+    var planet2 = planetObj.planetNames[randomInt(0,length)]
+
+    //select random substring of planet1 and planet2 and join them
+
+    var subPlanet1 = planet1.slice(randomInt(1, planet1.length))
+    var subPlanet2 = planet2.slice(randomInt(1, planet2.length))
+
+    var correctedName = correctPlanetName(subPlanet1 + subPlanet2)
+    return correctedName
+}
+
+function correctPlanetName(planetName) {
+
+    // remove anything in parentheses (these are comments from the site)
+    if ((planetName.indexOf("(")) != -1) {
+        var openParen = planetName.indexOf("(")
+        if (planetName.lastIndexOf(")") != -1) {
+            var closeParen = planetName.lastIndexOf(")")
+            planetName = planetName.substr(0,openParen) + planetName.substr(closeParen+1, planetName.length)
+        } else {
+            planetName = planetName.substr(0,openParen)
+        }
+    }
+
+    planetName = planetName.toLowerCase();
+    planetName = planetName.split(" ")
+    var newPlanetName = []
+
+    for (var i in planetName) {
+        var str = planetName[i]
+        //roman numeral matcher
+        var patt = /[[i]*[v]*]/
+        if (patt.test(str)) {
+            newPlanetName.push(str.toUpperCase())
+        } else {
+            str = str.charAt(0).toUpperCase() + str.slice(1)
+            if (str.length > 1) {
+                newPlanetName.unshift(str)
+            }
+        }
+    }
+
+    return (newPlanetName.join(" "))
+
+}
+
+function randomInt(min, max) {
+    var randInt = Number(Math.random().toString().slice(2))
+    return randInt % (max - min) + min
+}
+
+loadFile("planets.json", function(data) {
+    var planetName = randomPlanet(data)
+    console.log(planetName)
+})
--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-function addInput(block) {
+function addCampaign() {
+    var campaignFragment = $("<div class=campaignElement></div>")
 
+    campaignFragment.append($("<input type=number>Set map limit</input>"))
+    campaignFragment.append($("<button type='button' onClick=addPlayer()>Add player</button>"))
+
+    $("#campaignBlocks").append($(campaignFragment))
+}
+
+function addPlayer() {
+    var playerFragment = $("<div class=playerElement></div>")
+    playerFragment.append
+}
+
+
+function addInput(block) {
     // if there are any input elements in the div turn them into bullets
     var inputElements = $("#" + block + " input").each(function() {
         var inputString = $(this).val()
@@ -19,13 +33,43 @@ function addInput(block) {
 
 function addBlock() {
     // create the block element
-    var frag = document.createDocumentFragment();
+    var frag = $("<div class='campaignBlock'></div>")
+    var titleFrag = $("<h3></h3>")
+
     $(".inputElement").each(function() {
-        console.log($(this))
-        frag.appendChild($(this)[0])
+
+        // Put the title as the name field
+        var name = $(this).attr("name")
+        if (name == "Destination") {
+            var destination = $(this).children("li").html()
+            titleFrag.append(destination)
+        }
+
+        // Add elements
+        frag.append("<em>"+$(this).attr("name")+"</em>")
+        $(this).clone().attr("class", "campaignElement").appendTo(frag)
     })
+
+    $(".inputBlock .inputElement li").each(function() {
+        $(this).detach()
+    })
+
     console.log(frag)
-    $("#campaignBlocks")[0].appendChild(frag)
-    // detatch the unorder lists from entry fields
-    //$(".inputElement").detach()
+    $("#campaignBlocks").append(titleFrag)
+    $("#campaignBlocks").append(frag)
+    
+    // Refresh the accordion so it sees new elements
+    $("#campaignBlocks").accordion("refresh");
 }
+$(document).ready(function() {
+
+    $("#campaignBlock").accordion({
+        header: "h3",
+        collapsible: true,
+        active: false,
+        autoHeight: true,
+        autoActivate: true
+    });
+
+})
+
