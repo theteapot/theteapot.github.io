42fe2a6a1b9b0acae920ec87ee05ee2ff4b70156
theteapot <tin.teapot@gmail.com>
2017-07-05 00:24:41 +1200

    fixed nesting accordions, started canvas

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
-function addCampaign() {
-    var campaignFragment = $("<div class=campaignElement></div>")
-
-    campaignFragment.append($("<input type=number>Set map limit</input>"))
-    campaignFragment.append($("<button type='button' onClick=addPlayer()>Add player</button>"))
-
-    $("#campaignBlocks").append($(campaignFragment))
-}
-
-function addPlayer() {
-    var playerFragment = $("<div class=playerElement></div>")
-    playerFragment.append
-}
-
-
 function addInput(block) {
+
     // if there are any input elements in the div turn them into bullets
     var inputElements = $("#" + block + " input").each(function() {
         var inputString = $(this).val()
@@ -33,43 +19,40 @@ function addInput(block) {
 
 function addBlock() {
     // create the block element
-    var frag = $("<div class='campaignBlock'></div>")
-    var titleFrag = $("<h3></h3>")
-
+    var frag = document.createDocumentFragment();
     $(".inputElement").each(function() {
-
-        // Put the title as the name field
-        var name = $(this).attr("name")
-        if (name == "Destination") {
-            var destination = $(this).children("li").html()
-            titleFrag.append(destination)
-        }
-
-        // Add elements
-        frag.append("<em>"+$(this).attr("name")+"</em>")
-        $(this).clone().attr("class", "campaignElement").appendTo(frag)
+        console.log($(this))
+        
+        frag.appendChild($(this)[0])
     })
-
-    $(".inputBlock .inputElement li").each(function() {
-        $(this).detach()
-    })
-
     console.log(frag)
-    $("#campaignBlocks").append(titleFrag)
-    $("#campaignBlocks").append(frag)
+    $("#campaignBlocks")[0].appendChild(frag)
     
-    // Refresh the accordion so it sees new elements
-    $("#campaignBlocks").accordion("refresh");
 }
-$(document).ready(function() {
 
-    $("#campaignBlock").accordion({
-        header: "h3",
+$(document).ready(function() {
+    $("#campaignBlocks").accordion({
         collapsible: true,
         active: false,
-        autoHeight: true,
-        autoActivate: true
-    });
-
+        heightStyle: "content",
+        header: ".campaignBlock > h4"
+    })
+    $(".playerBlock").accordion({
+        collapsible: true,
+        active: false,
+        heightStyle: "content",
+        header: ".name"
+    })
+    $(".mapList").accordion({
+        collapsible: true,
+        active: false,
+        heightStyle: "content",
+        header: ".planet"
+    })
+    $(".map").accordion({
+        collapsible: true,
+        active: false,
+        heightStyle: "content",
+        header: ".mapName"
+    })
 })
-
