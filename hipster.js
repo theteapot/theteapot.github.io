af32b5e038112eebe3a22c03faa14d72563a8080
theteapot <tin.teapot@gmail.com>
2017-07-23 03:04:35 +1200

    added request class

--- hipster.js
+++ hipster.js
@@ -1,30 +1,54 @@
-function generateUrl(obj) {
-    var scope = "";
-    var checkNodes = document.querySelectorAll(".dataCheckbox");
-    
-    for (var index = 0; index < checkNodes.length; index++) {
-        var node = checkNodes[index]
-        if (node.checked) {
-            scope += node.value + " "
-        }
+
+/*              Classes             */
+
+class RequestFactory {
+    constructor(accessToken) {
+        this.accessToken = accessToken;
     }
 
-    scope = scope.trim();
-
-    var url = "https://accounts.spotify.com/authorize?client_id=e62d86020b9841e2bbc5c191b73c00ae" +
-    "&redirect_uri=https:%2F%2Ftheteapot.github.io%2Fhipster&scope="+scope+"&response_type=token"
-    console.log(url)
-
-    window.location.assign(url)
-  
+    createRequest(endpoint, callback) {
+        var request = new XMLHttpRequest();
+        request.addEventListener(callback);
+        request.open("GET", endpoint);
+        request.setRequestHeader("Authorization", "Bearer " + this.accessToken);
+        request.responseType = "json";
+        request.send();
+    }
 }
+
+/*          UI functions            */
 
 function handleUrl() {
     // Check if access is due to redirect from spotify
     // if this is the case there will be url parameters
     var urlParams = parseUrl(document.URL)
-    getUserInformation(urlParams)
+    console.log(Object.keys(urlParams), urlParams.constructor)
+    if (Object.keys(urlParams).length === 1 && urlParams.constructor === Object) { //check to see if url has no parameters
+        console.log("Auth page")
+        document.getElementById("beginPage").setAttribute("style", "visibility: visible");
+        document.getElementById("landingPage").setAttribute("style", "visibility: hidden");
+    } else {
+        console.log("Landing page")
+        requestApiObjects(urlParams.access_token);
+        document.getElementById("beginPage").setAttribute("style", "visibility: hidden");
+        document.getElementById("landingPage").setAttribute("style", "visibility: visible");
+    }
 }
+
+/*          Data handling functions             */
+
+function requestApiObjects(accessToken) {
+    var responseObj = {};
+    var requestFactory = new RequestFactory(accessToken);
+    requestFactory.createRequest("/v1/me/player/recently-played", recentlyPlayed)
+}
+
+function recentlyPlayed(data) {
+    console.log(JSON.stringify(data))
+}
+
+
+/*          Utility functions           */
 
 function parseUrl(url) {
     // Turns url string into JSON object with parameters as keys
